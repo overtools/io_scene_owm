74132b77ef377137f932ca112cb429f298773611
Naomi <yukimono6@gmail.com>
2018-08-17 15:54:42 +0100

    [owmat] move to library format

--- import_owmat.py
+++ import_owmat.py
@@ -73,146 +73,16 @@ def load_textures(texture, root, t):
         print("[import_owmat]: error loading texture: {}".format(e))
     return None
 
+OVERWATCH_NODEGROUP_PRIMARY_NAME = "OWM: Physically Based Shading"
+
 def create_overwatch_shader(tile=300): # Creates the Overwatch nodegroup, if it doesn't exist yet
-    if(bpy.data.node_groups.find("OverwatchGenerated") is not -1):
+    global OVERWATCH_NODEGROUP_PRIMARY_NAME
+    if(bpy.data.node_groups.find(OVERWATCH_NODEGROUP_PRIMARY_NAME) is not -1):
         return
-   
-    bpy.data.node_groups.new("OverwatchGenerated", "ShaderNodeTree")
-    ng = bpy.data.node_groups["OverwatchGenerated"]
- 
-    # Clearing nodegroup
-    ng.nodes.clear()
-    ng.inputs.clear()
-    ng.outputs.clear()
- 
-    # Creating Output node
-    nodeOutput = ng.nodes.new("NodeGroupOutput")
-    nodeOutput.location = (0, 0)
- 
-    # Adding outputs
-    ng.outputs.new("NodeSocketShader", "Shader")
- 
-    # Creating inputs and setting their default values - Input node is created at the end.
-    inputCol = ng.inputs.new("NodeSocketColor", "Color")
-    inputOpacity = ng.inputs.new("NodeSocketFloat", "Opacity")
-    inputOpacity.default_value = 1
-    inputSpec = ng.inputs.new("NodeSocketColor", "OWSpecMap")
-    inputSpec.default_value = (0, 0, 0, 1)
-    inputNormal = ng.inputs.new("NodeSocketColor", "Normal")
-    inputNormal.default_value = (.5, .5, 1, 1)
-    inputNormalStrength = ng.inputs.new("NodeSocketFloat", "Normal Strength")
-    inputNormalStrength.default_value = 1
-    inputEmission = ng.inputs.new("NodeSocketFloat", "Emission Mask")
-    inputEmissionStrength = ng.inputs.new("NodeSocketFloat", "Emission Strength")
-    inputEmissionStrength.default_value = 1
- 
-    ### SPAWNING NODES ###
-    nodeMixTransp = ng.nodes.new("ShaderNodeMixShader")
-    nodeMixTransp.location = (-tile, 0)
- 
-    nodeMixEmission = ng.nodes.new("ShaderNodeMixShader")
-    nodeMixEmission.location = (-tile*2, 0)
- 
-    nodePrincipled = ng.nodes.new("ShaderNodeBsdfPrincipled")
-    nodePrincipled.location = (-tile*3, 0)
- 
-    nodeTransp = ng.nodes.new("ShaderNodeBsdfTransparent")
-    nodeTransp.location = (-tile*3, 200)
- 
-    nodeEmission = ng.nodes.new("ShaderNodeEmission")
-    nodeEmission.location = (-tile*3, -600)
- 
-    nodeTangent = ng.nodes.new("ShaderNodeTangent")
-    nodeTangent.location = (-tile*4, -tile*3)
-    nodeTangent.direction_type = 'UV_MAP'
- 
-    nodeNormal = ng.nodes.new("ShaderNodeNormalMap")
-    nodeNormal.location = (-tile*4, -tile*2)
-    
-    nodeCombineRGBNormal = ng.nodes.new("ShaderNodeCombineRGB")
-    nodeCombineRGBNormal.location = (-tile*5, -tile*2)
-    nodeCombineRGBNormal.inputs[2].default_value = 1    # Discarding normal map Z channel and setting it to 1.
-    
-    nodeInvertNormalGreen = ng.nodes.new("ShaderNodeInvert")
-    nodeInvertNormalGreen.location = (-tile*6, -tile*2)
-    
-    nodeSeparateRGBNormal = ng.nodes.new("ShaderNodeSeparateRGB")
-    nodeSeparateRGBNormal.location = (-tile*7, -tile*2)
- 
-    ## SPECULAR SETUP ##
-    nodeMathSpec2 = ng.nodes.new("ShaderNodeMath")
-    nodeMathSpec2.location = (-tile*4, 0)
-    nodeMathSpec2.operation = 'MULTIPLY'
-    nodeMathSpec2.inputs[1].default_value = 0.5
- 
-    nodeMathSpec = ng.nodes.new("ShaderNodeMath")
-    nodeMathSpec.location = (-tile*5, 0)
-    nodeMathSpec.operation = 'MULTIPLY'
-    nodeMathSpec.inputs[1].default_value = 2
-    nodeMathSpec.use_clamp = True
- 
-    nodeMathMetal2 = ng.nodes.new("ShaderNodeMath")
-    nodeMathMetal2.location = (-tile*4, -tile/2)
-    nodeMathMetal2.operation = 'MULTIPLY'
-    nodeMathMetal2.inputs[1].default_value = 2
- 
-    nodeMathMetal = ng.nodes.new("ShaderNodeMath")
-    nodeMathMetal.location = (-tile*5, -tile/2)
-    nodeMathMetal.operation = 'SUBTRACT'
-    nodeMathMetal.inputs[1].default_value = 0.5
-    nodeMathMetal.use_clamp = True
- 
-    nodeGamma = ng.nodes.new("ShaderNodeGamma")
-    nodeGamma.location = (-tile*4, -tile)
- 
-    nodeInvertRoughness = ng.nodes.new("ShaderNodeInvert")
-    nodeInvertRoughness.location = (-tile*5, -tile)
- 
-    nodeSeparateRGBSpec = ng.nodes.new("ShaderNodeSeparateRGB")
-    nodeSeparateRGBSpec.location = (-tile*6, -tile/2)
- 
-    # Input node
-    nodeInput = ng.nodes.new("NodeGroupInput")
-    nodeInput.location = (-tile*8, -tile/2)
- 
-    ### CONNECTING NODES ###
-    links = ng.links
-    links.new(nodeInput.outputs["Color"], nodePrincipled.inputs["Base Color"])
-    links.new(nodeInput.outputs["Opacity"], nodeMixTransp.inputs["Fac"])
-    links.new(nodeInput.outputs["OWSpecMap"], nodeSeparateRGBSpec.inputs["Image"])
-    links.new(nodeInput.outputs["Emission Mask"], nodeMixEmission.inputs["Fac"])
-    links.new(nodeInput.outputs["Emission Strength"], nodeEmission.inputs["Strength"])
-    links.new(nodeInput.outputs["Normal"], nodeSeparateRGBNormal.inputs[0])
-    links.new(nodeInput.outputs['Normal Strength'], nodeNormal.inputs["Strength"])
-    
-    links.new(nodeSeparateRGBNormal.outputs[0], nodeCombineRGBNormal.inputs[0])
-    links.new(nodeSeparateRGBNormal.outputs[1], nodeInvertNormalGreen.inputs["Color"])
-    links.new(nodeInvertNormalGreen.outputs[0], nodeCombineRGBNormal.inputs[1])
-    links.new(nodeCombineRGBNormal.outputs[0], nodeNormal.inputs["Color"])
- 
-    links.new(nodeSeparateRGBSpec.outputs["R"], nodeMathSpec.inputs[0])
-    links.new(nodeSeparateRGBSpec.outputs["R"], nodeMathMetal.inputs[0])
-    links.new(nodeSeparateRGBSpec.outputs["G"], nodeInvertRoughness.inputs["Color"])
- 
-    links.new(nodeMathSpec.outputs[0], nodeMathSpec2.inputs[0])
-    links.new(nodeMathMetal.outputs[0], nodeMathMetal2.inputs[0])
-    links.new(nodeInvertRoughness.outputs[0], nodeGamma.inputs[0])
- 
-    links.new(nodeMathSpec2.outputs[0], nodePrincipled.inputs["Specular"])
-    links.new(nodeMathMetal2.outputs[0], nodePrincipled.inputs["Metallic"])
-    links.new(nodeGamma.outputs[0], nodePrincipled.inputs["Roughness"])
-    links.new(nodeNormal.outputs[0], nodePrincipled.inputs["Normal"])
-    links.new(nodeTangent.outputs[0], nodePrincipled.inputs["Tangent"])
- 
-    links.new(nodeInput.outputs["Color"], nodeEmission.inputs["Color"])
- 
-    links.new(nodePrincipled.outputs[0], nodeMixEmission.inputs[1])
-    links.new(nodeEmission.outputs[0], nodeMixEmission.inputs[2])
-    links.new(nodeTransp.outputs[0], nodeMixTransp.inputs[1])
-    links.new(nodeMixEmission.outputs[0], nodeMixTransp.inputs[2])
- 
-    links.new(nodeMixTransp.outputs[0], nodeOutput.inputs[0])
-    print("Creating Overwatch nodegroup has finished.")
+    path = os.path.join(os.path.dirname(__file__), "library.blend")
+    with bpy.data.libraries.load(path, False) as (data_from, data_to):
+        data_to.node_groups = [node_name for node_name in data_from.node_groups if not node_name in bpy.data.node_groups and node_name.startswith("OWM")]
+        print("[import_owmat] imported node groups: %s" % (', '.join(data_to.node_groups)))
 
 def read(filename, prefix = '', importNormal = True, importEffect = True):
     root, file = os.path.split(filename)
@@ -234,7 +104,8 @@ def read(filename, prefix = '', importNormal = True, importEffect = True):
 
     return (t, m)
 
-def process_material_Cycles(material, prefix, root, t):    
+def process_material_Cycles(material, prefix, root, t):
+    global OVERWATCH_NODEGROUP_PRIMARY_NAME
     mat = bpy.data.materials.new('%s%016X' % (prefix, material.key))
 
     if material.shader != 0:
@@ -256,7 +127,7 @@ def process_material_Cycles(material, prefix, root, t):
  
     # Create Overwatch NodeGroup Instance
     nodeOverwatch = nodes.new('ShaderNodeGroup')
-    nodeOverwatch.node_tree = bpy.data.node_groups["OverwatchGenerated"]
+    nodeOverwatch.node_tree = bpy.data.node_groups[OVERWATCH_NODEGROUP_PRIMARY_NAME]
     nodeOverwatch.location = (0, 0)
     nodeOverwatch.width = 250
     links.new(nodeOverwatch.outputs[0], material_output.inputs[0])
@@ -288,24 +159,24 @@ def process_material_Cycles(material, prefix, root, t):
         tt = owm_types.TextureTypes
         if typ == tt['DiffuseAO'] or typ == tt['DiffuseOpacity'] or typ == tt['DiffuseBlack'] \
             or typ == tt['DiffusePlant'] or typ == tt['DiffuseFlag'] or typ == tt['Diffuse2']:
-            links.new(nodeTex.outputs["Color"], nodeOverwatch.inputs["Color"])
+            links.new(nodeTex.outputs["Color"], nodeOverwatch.inputs["Base Color"])
             nodeTex.color_space = 'COLOR'
             nodeTex.name = "Color"
         if typ == tt['DiffuseAO']:
             nodeTex.image.use_alpha = False
         if typ == tt['DiffuseOpacity']:
-            links.new(nodeTex.outputs["Alpha"], nodeOverwatch.inputs["Opacity"])
+            links.new(nodeTex.outputs["Alpha"], nodeOverwatch.inputs["Opacity Map"])
         if typ == tt['DiffuseBlack']:
-            links.new(nodeTex.outputs["Color"], nodeOverwatch.inputs["Opacity"])
-            links.new(nodeTex.outputs["Color"], nodeOverwatch.inputs["Emission Mask"])
+            links.new(nodeTex.outputs["Color"], nodeOverwatch.inputs["Opacity Map"])
+            links.new(nodeTex.outputs["Color"], nodeOverwatch.inputs["Emission Map"])
         if typ == tt['Opacity'] or typ == tt['Opacity2']:
-            links.new(nodeTex.outputs["Color"], nodeOverwatch.inputs["Opacity"])
+            links.new(nodeTex.outputs["Color"], nodeOverwatch.inputs["Opacity Map"])
         if typ == tt['Tertiary'] or typ == tt['Tertiary2']:
-            links.new(nodeTex.outputs["Color"], nodeOverwatch.inputs["OWSpecMap"])
+            links.new(nodeTex.outputs["Color"], nodeOverwatch.inputs["Packed PBR"])
         if typ == tt['Emission'] or typ == tt['Emission2'] or typ == tt['Emission3']:
-            links.new(nodeTex.outputs["Color"], nodeOverwatch.inputs["Emission Mask"])
+            links.new(nodeTex.outputs["Color"], nodeOverwatch.inputs["Emission Map"])
         if typ == tt['Normal'] or typ == tt['HairNormal'] or typ == tt['CorneaNormal']:
-            links.new(nodeTex.outputs["Color"], nodeOverwatch.inputs["Normal"])
+            links.new(nodeTex.outputs["Color"], nodeOverwatch.inputs["RG Packed Normal"])
     
     nodes.active = nodes.get("Color")
     
