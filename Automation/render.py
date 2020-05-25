75c59b46062e20e5ef02d1cb7c39d3a66204402b
Naomi <614231+healingbrew@users.noreply.github.com>
2020-05-25 07:49:16 +0100

    [Automation] update render script

--- Automation/render.py
+++ Automation/render.py
@@ -1,15 +1,19 @@
 # blender -P render.py -- <owentity_path> <owmat_path> <render_path>
 
-import bpy, sys, os, mathutils
+import bpy, sys, os, math
+from mathutils import Vector
+import io_scene_owm
 
 entity_path = sys.argv[-3]
 material_path = sys.argv[-2]
 render_path = sys.argv[-1]
 
 objects = bpy.context.scene.collection.objects
-
-for object in objects:
-    objects.unlink(object)
+for obj in objects:
+    objects.unlink(obj)
+camera_data = bpy.data.cameras.new("Camera")
+camera = bpy.data.objects.new("Camera", camera_data)
+objects.link(camera)
 
 def bind_materials(meshes, data, materials):
     if materials == None:
@@ -21,30 +25,79 @@ def bind_materials(meshes, data, materials):
             mesh.materials.clear()
             mesh.materials.append(materials[1][mesh_data.materialKey])
 
-(entity, entity_data, entity_model)  = bpy.ops.import_mesh.overtools_entity(filepath=entity_path)
+owentity_settings = io_scene_owm.owm_types.OWSettings(
+    entity_path, # self.filepath,
+    0, # self.uvDisplX,
+    0, # self.uvDisplY,
+    False, # self.autoIk,
+    True, # self.importNormals,
+    True,  # self.importEmpties
+    False, # self.importMaterial,
+    True,  # self.importSkeleton
+    True, # self.importColor
+)
+
+light1_data = bpy.data.lights.new("Light A", "AREA")
+light1 = bpy.data.objects.new("Light A", light1_data)
+objects.link(light1)
+light2_data = bpy.data.lights.new("Light B", "AREA")
+light2 = bpy.data.objects.new("Light B", light2_data)
+objects.link(light2)
+
+io_scene_owm.owm_types.update_data()
+(entity_root, entity_data, model) = io_scene_owm.import_owentity.read(owentity_settings, False)
 if material_path != "null":
-    material = bpy.ops.import_material.overtools_material(filepath=material_path)
-    bind_materials(entity_model[2], entity_model[4], material)
+    material = io_scene_owm.import_owmat.read(material_path, '')
+    bind_materials(model[2], model[4], material)
 
-dg = bpy.context.evaluated_depsgraph_get()
+max_top = [0, 0, 0]
+min_top = [0, 0, 0]
+for i, obj in enumerate(model[2]):
+    for j in range(0, 8):
+        for k in range(0, 3):
+            max_top[k] = max([max_top[k], obj.bound_box[j][k]])
+            min_top[k] = min([min_top[k], obj.bound_box[j][k]])
+size_base = (Vector(max_top) - Vector(min_top)).length
+size = size_base * 5
+light1_data.energy = size_base * 20
+light2_data.energy = size_base * 10
+camera_data.lens = size_base * 200
 
-camera = camera = bpy.data.objects.new("Camera", bpy.data.cameras.new("Camera"))
-objects.link(camera)
-
-# set up camera
-
-for object in enumerate(entity_model[2]):
-    corner = object.location + mathutils.Vector(object.bound_box[5][:]) + mathutils.Vector(2, -2, 2)
+for i, obj in enumerate(model[2]):
+    max_extent = 0
+    for j in range(0, 8):
+        for k in range(0, 3):
+            max_extent = max([max_extent, obj.bound_box[j][k]])
+    max_extent *= size
+    corner = obj.location + Vector((max_extent * size, -max_extent * size, max_extent * (size_base + 1.0)))
+    side = obj.location + Vector((max_extent, 0, max_extent * size_base))
     if camera.location < corner:
         camera.location = corner 
+        light1.location = corner
+    if light2.location < side:
+        light2.location = side
 
 bpy.context.view_layer.objects.active = camera
 bpy.ops.object.constraint_add(type='TRACK_TO')
-bpy.context.object.constraints["Track To"].target = entity
+bpy.context.object.constraints["Track To"].target = entity_root
 bpy.context.object.constraints["Track To"].track_axis = 'TRACK_NEGATIVE_Z'
 bpy.context.object.constraints["Track To"].up_axis = 'UP_Y'
+bpy.context.view_layer.objects.active = light1
+bpy.ops.object.constraint_add(type='TRACK_TO')
+bpy.context.object.constraints["Track To"].target = entity_root
+bpy.context.object.constraints["Track To"].track_axis = 'TRACK_NEGATIVE_Z'
+bpy.context.object.constraints["Track To"].up_axis = 'UP_Y'
+bpy.context.view_layer.objects.active = light2
+bpy.ops.object.constraint_add(type='TRACK_TO')
+bpy.context.object.constraints["Track To"].target = entity_root
+bpy.context.object.constraints["Track To"].track_axis = 'TRACK_NEGATIVE_Z'
+bpy.context.object.constraints["Track To"].up_axis = 'UP_Y'
+bpy.context.scene.camera = camera
+bpy.context.scene.render.resolution_x = 2048
+bpy.context.scene.render.resolution_y = 2048
+bpy.context.scene.render.film_transparent = True
 
-dg.update()
+bpy.context.view_layer.update()
 
-# bpy.context.scene.render.filepath = render_path
-# bpy.ops.render.render(write_still=True)
+bpy.context.scene.render.filepath = render_path
+bpy.ops.render.render(write_still=True)
