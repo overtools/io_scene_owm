24c17adfabac74c1410b055f58469bde1218b2b2
Lilith Naomi <yretenai@gmail.com>
2021-08-23 18:14:05 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ from . import manager
 bl_info = {
     'name': 'OWM Import',
     'author': 'overtools',
-    'version': (2, 4, 0),
+    'version': (2, 4, 1),
     'blender': (2, 92, 0),
     'location': 'File > Import > OWM',
     'description': 'Import exported assets from TankLib or DataTool',
