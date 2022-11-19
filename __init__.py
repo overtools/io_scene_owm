d9f73e34cdf7371c21b3f0ee8a518de3dd118d25
Aini <rafacaval@icloud.com>
2022-11-19 17:52:50 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     'name': 'OWM Import',
     'author': 'overtools',
-    'version': (3, 1, 0),
+    'version': (3, 1, 1),
     'blender': (3, 2, 0),
     'location': 'File > Import > OWM',
     'description': 'Import exported assets from TankLib or DataTool',
